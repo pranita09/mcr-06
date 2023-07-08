15c54474d2b2afe08acf9607b708cb4614bef2b4
Pranita <pfulsundar8@gmail.com>
2023-07-08 14:56:50 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <div className="flex justify-center items-center p-4">
-      <h1 className="text-3xl font-bold text-red-500">Home</h1>
+      <h1 className="text-3xl font-bold text-red-500">Home Page</h1>
     </div>
   );
 };
